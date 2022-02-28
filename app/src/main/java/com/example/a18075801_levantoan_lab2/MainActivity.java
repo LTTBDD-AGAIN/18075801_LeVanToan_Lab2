aa395210e4459c4eb48a800a64c219190752cce5
Le Van Toan <lvtoan.it2000@gmail.com>
2022-02-28 15:34:57 +0700

    nop bai lab 2

---
+++ app/src/main/java/com/example/a18075801_levantoan_lab2/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.a18075801_levantoan_lab2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnLogin;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnLogin = findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"Hello World", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/a18075801_levantoan_lab2/PasswordGenerator.java
@@ -0,0 +1,14 @@
+package com.example.a18075801_levantoan_lab2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PasswordGenerator extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_password_generator);
+    }
+}
